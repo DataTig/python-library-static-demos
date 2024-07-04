bf7ab8556f90f6cf416cc07bc35ff7aeb74a7e43
James <james@jarofgreen.co.uk>
2024-07-04 13:52:26 +0100

    Add Versioned build

--- build.sh
+++ build.sh
@@ -13,10 +13,18 @@ fi
 
 if [ -d gitrepos/datatig-website ]; then
   cd gitrepos/datatig-website
+  # Note this only updates the current branch, not any new ones.
+  # But as this is only used in local dev at the moment we can live with that.
   git pull
   cd ../..
 else
   git clone --branch main https://github.com/DataTig/datatig.github.io.git gitrepos/datatig-website
+  cd gitrepos/datatig-website
+  # This for loop is from https://stackoverflow.com/a/4754797
+  for branch in $(git branch --all | grep '^\s*remotes'| egrep --invert-match '(:?HEAD|main)$'); do
+    git branch --track "${branch##*/}" "$branch"
+  done
+  cd ../..
 fi
 
 echo "Build org-id-register"
@@ -26,3 +34,4 @@ python -m datatig.cli build gitrepos/org-id-register/  --staticsiteoutput output
 echo "Build datatig-website"
 mkdir -p output/datatig-website/branch
 python -m datatig.cli build gitrepos/datatig-website/  --staticsiteoutput output/datatig-website/branch/main --staticsiteurl=/datatig-website/branch/main
+python -m datatig.cli versionedbuild gitrepos/datatig-website/  --allbranches --defaultref main --staticsiteoutput output/datatig-website/versioned --staticsiteurl=/datatig-website/versioned
